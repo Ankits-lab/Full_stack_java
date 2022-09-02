aa5b5d942ff661cd6786c29bacf2f8efcfcc7fa1
keneankit01 <keneankit01@gmail.com>
2022-09-02 12:34:57 +0530

    inital spring boot learning

---
+++ java/keneankit/src/main/java/com/keneankit/keneankit/KeneankitApplication.java
@@ -0,0 +1,13 @@
+package com.keneankit.keneankit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeneankitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeneankitApplication.class, args);
+	}
+
+}
---
+++ java/keneankit/src/test/java/com/keneankit/keneankit/KeneankitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keneankit.keneankit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeneankitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
